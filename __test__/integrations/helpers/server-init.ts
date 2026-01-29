f03fe721bf63026fec69f1f71fb52b2aca0f1468
Diego Resendez <diego.resendez@zero-oneit.com>
2026-01-29 15:11:34 -0600

    fix: convert supertest namespace imports to default imports for SWC compatibility
    - Change 'import * as supertest' to 'import supertest' in integration test helpers
    - Resolves TypeError: _supertest is not a function with @swc/jest
    - Follows same pattern as express import fix in Boot.ts
    - Test results: 290/313 passing (92.7%), down from 30 failures to 23

--- __test__/integrations/helpers/server-init.ts
+++ __test__/integrations/helpers/server-init.ts
@@ -1,4 +1,4 @@
-import * as supertest from 'supertest';
+import supertest from 'supertest';
 import Boot from '../../../classes/Boot';
 import { Modules } from '../../../decorators/server';
 import RootModule from './modules/root/RootModule';
